e2ab83bf2b4e1c3d8a4e74b67dd569d44d649c86
Austin Vuong <austin.vuong00@gmail.com>
2020-01-30 16:51:58 +1100

    Attached Google Analytics

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.scss';
 import Main from './containers/MainContainer';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 function App() {
+  ReactGA.initialize('UA-150410010-2');
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <>
     <body className="container">
-    <Router>
-      <Route exact path='/' component={Main} />
-    </Router>
+    <Main />
     </body>
     </>
   );
