35218f729db06bb11292e6cdc8c1dcd9ec642b63
Austin Vuong <austin.vuong00@gmail.com>
2020-01-30 16:27:22 +1100

    Updates

---
+++ precache-manifest.6b250b3280d6cd4ac4144aaebd1057ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4cc25fcdf4a539b2f8ff3cf2dbe7be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ce2b7edd9fd4a26488",
+    "url": "/static/css/main.b8ec35e4.chunk.css"
+  },
+  {
+    "revision": "7874e68bfa32b8617fb8",
+    "url": "/static/js/2.a85eb638.chunk.js"
+  },
+  {
+    "revision": "c5ce2b7edd9fd4a26488",
+    "url": "/static/js/main.2eb3d73a.chunk.js"
+  },
+  {
+    "revision": "f9c055dd03423cdeba44",
+    "url": "/static/js/runtime~main.c58b36c0.js"
+  }
+]);
