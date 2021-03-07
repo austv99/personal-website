5422394ba2b61dad0a18b8b7e717b43ef931055b
Austin Vuong <austin.vuong00@gmail.com>
2021-03-07 22:05:16 +1100

    Updated my profile

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -10,42 +10,36 @@ const MainContainer = (props) => {
     const AnimateGreeting = styled.div `animation: 2s ${greetingAnimation};`;
     return (
         <div className="wrapper">
-            {/* <div className="socials">
-                <a href="http://github.com/austv99"><img src="assets/github.svg" alt="" /></a>
-                <a href="http://www.linkedin.com/in/austin-vuong"><img src="assets/linkedin.svg" alt=""/></a>
-            </div> */}
                 <AnimateImage>
                     <img className="profile__pic" src="assets/profile_s.png" alt=""></img>
                 </AnimateImage>
-                <h2 className="quote">coffee, code and 808s</h2>
                 <AnimateGreeting>
                     <div className="greet">
                         <h1>Hey!</h1>
                     </div>
                 </AnimateGreeting>
                 <h2 className="me">I'm Austin Vuong</h2>
-                <h2 className="uni">Penultimate Software Engineering @ UNSW</h2>
-                <h2 className="intro">An aspiring full stack engineer.
+                <h2 className="uni">Final Year Software Engineering @ UNSW</h2>
+                <h2 className="intro">An aspiring front end software engineer.
                 When I’m not stuck in Visual Studio Code, I enjoy music and photography.</h2>
                 <div className="body">
                 <h2 className="heading">What am I busy with now?</h2>
                 <div className="text__body">
-                    <h2>I'm currently an intern at Atlassian and having great fun!</h2>
-                    <h2>Other than that, I’m in my penultimate year for Software Engineering @ UNSW with a Distinction average.</h2>
+                    <h2>I've just finished interning at Atlassian and had great fun working there and learnt tons!</h2>
+                    <h2>Other than that, I’m in my final year for Software Engineering @ UNSW with a Distinction average.</h2>
                     <h2>Oh, and I dabble in photography. Still learning but we all start somewhere right?</h2>
                 </div>
                 <h2 className="heading">What languages do you spea... I mean code in?</h2>
                 <div className="text__body">
-                    <h2>Loving React currently.</h2>
+                    <h2>Loving React and Javascript currently.</h2>
                     <h2>Java is great.</h2>
                     <h2>Python and C too.</h2>
+                    <h2>I've worked with Perl and Shell Script</h2>
+                    <h2>And Spring Boot too!</h2>
                     <h2>I have a positive relation(ship) with PostgreSQL.</h2>
                     <h2>I'm planning on trying out Electron and Swift too!</h2>
+                    <h2>And of course, I'm always keen to keep learning new technologies!</h2>
                 </div> 
-                {/* <div className="cheeky">
-                    <h2>*yep I’ll show myself out after that one <span role="img" aria-labelledby="Door">🚪</span>*</h2>
-                    <h2>ps. I speak English and a bit of Vietnamese.</h2>
-                </div> */}
                 <h2 className="heading">My <a href="http://github.com/austv99">github</a>!</h2> 
                 <div className="text__body">
                     <h2>I’m an eager individual who seeks to learn and, once I know what I’m doing, pass it on and teach!</h2>
